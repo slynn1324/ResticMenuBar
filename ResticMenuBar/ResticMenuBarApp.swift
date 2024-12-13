52d3d6e1a9d913d3f0c0b83181e31f2a6adbcff2
slynn1324 <slynn1324@gmail.com>
2024-12-13 17:21:45 -0600

    Initial Commit

---
+++ ResticMenuBar/ResticMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResticMenuBarApp.swift
+//  ResticMenuBar
+//
+//  Created by Stephen Lynn on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ResticMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
